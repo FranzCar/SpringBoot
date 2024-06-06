eb3fdca285dba94122e5c8939f7e2af1205529a7
Franz Carrillo <franzluiz10@gmail.com>
2024-06-05 21:02:35 -0400

    Primer commit del proyecto Spring Boot

---
+++ src/main/java/org/umss/spring_course/restful_web_service_example_01/RestfulWebServiceExample01Application.java
@@ -0,0 +1,13 @@
+package org.umss.spring_course.restful_web_service_example_01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfulWebServiceExample01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestfulWebServiceExample01Application.class, args);
+	}
+
+}
---
+++ src/test/java/org/umss/spring_course/restful_web_service_example_01/RestfulWebServiceExample01ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.umss.spring_course.restful_web_service_example_01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestfulWebServiceExample01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
